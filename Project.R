0723cbd2a4441cf40642b002b1a68738988df75e
yildizdr <155862764+yildizdr@users.noreply.github.com>
2024-10-22 14:13:19 -0400

    Update Project.R

--- Project.R
+++ Project.R
@@ -3,6 +3,38 @@
 # Yildiz Derinkok
 # Fall 2024
 
+### Create Data frames with variables of interest
+df_act <- read.delim("atusact_0323.dat", sep=",")
+df_resp <- read.delim("atusresp_0323.dat", sep=",")
+
+df_act <- df_act[c("TUCASEID", "TEWHERE", "TUCUMDUR", "TUSTARTTIM", "TUSTOPTIME")]
+df_resp <- df_resp[c("TUCASEID", "TUDIARYDATE", "TELFS", "TRDTOCC1", "TROHHCHILD", "TESCHENR")]
+
+df <- merge(df_act, df_resp, by="TUCASEID") #merge dataframes
+
+
+
+# Clean date variables
+df$TUSTARTTIM <- hms(df$TUSTARTTIM)
+df$TUSTOPTIME <- hms(df$TUSTOPTIME)
+df$TUDIARYDATE <- ymd(df$TUDIARYDATE)
+
+# Make each person's day 24hrs
+
+
+# Only keep activities where people are outside the home
+#df <- df[df$TEWHERE != 1,] #1: at home or yard
+#df <- df[df$TEWHERE != -1,] #-1: where info not collected
+
+# Calculate total time spent outside for each person
+d <- df %>%
+  group_by(TUCASEID) %>%
+  select(TUCASEID, TUCUMDUR, TUSTARTTIM, TUSTOPTIME) %>%
+  mutate(TotalTime = sum(TUCUMDUR))
+
+# Calculate median time spent outside for each date
+
+
 
 
 
